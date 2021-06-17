87c8e4e48d36acdb0ee1d5c770c0401bf610391c
Bagas Pramono <bagaspramono78@gmail.com>
2021-06-17 12:30:45 +0700

    update

--- application/views/forgot.php
+++ application/views/forgot.php
@@ -29,21 +29,46 @@
     <div class="page login-page" style="background-image: url('<?php echo base_url() ?>asset/gambar/hexagon.png');">
       <div class="container d-flex align-items-center">
         <div class="form-holder has-shadow" style="box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.2), 0 3px 17px 0 rgba(0, 0, 0, 0.19);">
-          <div class="row">
+          <div class="row"> 
             <!-- Logo & Information Panel-->
-            <div class="col-lg-6">
-              <div class="info align-items-center">
+            <div class="col-lg-6" style="background-color: #fff;">
+             <!--  <div class="info align-items-center">
                 <div class="content" align="center">
-                  <div class="logo">
-                    <h1>DATA UMKM</h1>
+                  <div class="logo"> -->
+                    
+                    <!-- <img style="width: 90%;" src="<?php echo base_url('asset/gambar/setting/').@$setting['setting_logo'] ?>"> -->
 
-                    <br/>
-                    <br/>
-                    <img style="width: 90%;" src="<?php echo base_url('asset/gambar/setting/').@$setting['setting_logo'] ?>">
-
-                  </div>
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img class="d-block w-100" style="height: 450px;" src="<?php echo base_url('asset/gambar/setting/').@$setting['setting_slide_1'] ?>" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                          <img class="d-block w-100" style="height: 450px;" src="<?php echo base_url('asset/gambar/setting/').@$setting['setting_slide_2'] ?>" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                          <img class="d-block w-100" style="height: 450px;" src="<?php echo base_url('asset/gambar/setting/').@$setting['setting_slide_3'] ?>" alt="Third slide">
+                        </div>
+                        <div class="carousel-item">
+                          <img class="d-block w-100" style="height: 450px;" src="<?php echo base_url('asset/gambar/setting/').@$setting['setting_slide_4'] ?>" alt="Four slide">
+                        </div>
+                        <div class="carousel-item">
+                          <img class="d-block w-100" style="height: 450px;" src="<?php echo base_url('asset/gambar/setting/').@$setting['setting_slide_5'] ?>" alt="Five slide">
+                        </div>
+                      </div>
+                      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
+ 
+                 <!--  </div>
                 </div>
-              </div>
+              </div> -->
             </div>
             <!-- Form Panel    -->
             <div class="col-lg-6 bg-white">
